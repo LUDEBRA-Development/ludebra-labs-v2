1afde7d3b66df91a7aa4aa6618b62eaa37b4c42b
mibichoandafugao <augustohenao1234@gmail.com>
2025-06-16 12:07:04 -0500

    Framer motion added, pages structure updated

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from '../components/pagewrapper'
+
+export default function Login() {
+  return (
+    <PageWrapper>
+      <div>
+        <h1>this is a login</h1>
+      </div>
+    </PageWrapper>
+  )
+}
